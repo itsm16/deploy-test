60754489e6adc08052c42cfbdff10b10c849e12b
itsm16 <mrinalg2k1@gmail.com>
2026-06-17 15:15:50 +0530

    chore: yml file command fixes for docker

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ import express from 'express'
 const app = express()
 
 app.get('/', (req, res) => {
-  res.json({ message: 'server is running v1.1', status: "docker-test-fix" })
+  res.json({ message: 'server is running v1.2', status: "docker-test-fix" })
 })
 
 app.listen(3000, () => {
